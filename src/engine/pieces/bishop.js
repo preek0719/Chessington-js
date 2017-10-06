1ff00a8fa1958f548b55ae066fffc07538f5cd6f
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:55:29 +0100

    Red 14

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -2,50 +2,79 @@ import Piece from './piece';
 import Square from "../square";
 import Player from "../player";
 import GameSettings from "../gameSettings"
-
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
     }
-
     getAvailableMoves(board) {
         let location = board.findPiece(this);
         let availableMoves = [];
-
-
         //Forward Right Diagonal
         let row = location.row + 1;
         let col = location.col + 1;
-        for (let i = row, j = col; i <= 7, j <= 7; i++, j++) {
-            availableMoves.push(Square.at(i, j));
+        for (let i = row, j = col; i <= 7 && j <= 7; i++, j++) {   
+            let curPiece = board.getPiece(Square.at(i, j));
+            if (curPiece == undefined) {
+                availableMoves.push(Square.at(i, j));
+            }             
+            else if (curPiece != undefined && curPiece.player != this.player && curPiece.constructor.name != 'King') {
+                availableMoves.push(Square.at(i, j));
+                break;
+            }            
+            else if (curPiece != undefined && curPiece.player == this.player)  {                
+                break;
+            }
+            
         }
-
-
         //Forward Left Diagonal
-        row = location.row + 1;
-        col = location.col - 1;
-        for (let i = row, j = col; i <= 7, j >= 0; i++, j--) {
-            availableMoves.push(Square.at(i, j));
-        }
-
-
+        row = location.row + 1;  //5 
+        col = location.col - 1; //3
+        for (let i = row, j = col; i <= 7 && j >= 0; i++, j--) {            
+            let curPiece = board.getPiece(Square.at(i, j));
+            if (curPiece == undefined) {
+                availableMoves.push(Square.at(i, j));
+            }             
+            else if (curPiece != undefined && curPiece.player != this.player && curPiece.constructor.name != 'King') {
+                availableMoves.push(Square.at(i, j));
+                break;
+            }            
+            else if (curPiece != undefined && curPiece.player == this.player)  {                
+                break;
+            }
+            }
+    
         //Backward Right Diagonal
         row = location.row - 1;
         col = location.col + 1
         for (let i = row, j = col; i >= 0; i--, j++) {
-            availableMoves.push(Square.at(i, j));
-        }
-
+            let curPiece = board.getPiece(Square.at(i, j));
+            if (curPiece == undefined) {
+                availableMoves.push(Square.at(i, j));
+            }             
+            else if (curPiece != undefined && curPiece.player != this.player && curPiece.constructor.name != 'King') {
+                availableMoves.push(Square.at(i, j));
+                break;
+            }            
+            else if (curPiece != undefined && curPiece.player == this.player)  {                
+                break;
+            }
+        }   
         //Backward Left Diagonal
         row = location.row - 1;
         col = location.col - 1
-        for (let i = row, j = col; i>0, j>0; i--, j--) {
-            availableMoves.push(Square.at(i, j));
+        for (let i = row, j = col; i > 0, j > 0; i--, j--) {
+            let curPiece = board.getPiece(Square.at(i, j));
+            if (curPiece == undefined) {
+                availableMoves.push(Square.at(i, j));
+            }             
+            else if (curPiece != undefined && curPiece.player != this.player && curPiece.constructor.name != 'King') {
+                availableMoves.push(Square.at(i, j));
+                break;
+            }            
+            else if (curPiece != undefined && curPiece.player == this.player)  {                
+                break;
+            }
         }
-
-
-        console.log(availableMoves)
-
         return availableMoves;
     }
 }
