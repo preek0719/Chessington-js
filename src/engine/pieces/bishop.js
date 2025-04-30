3d2dff2b5de4cc119b4022ae4ee95d719bd725bf
preek0719 <preetha.krishnan@softwire.com>
2025-04-30 19:53:01 +0100

    added code for Red-4 commit

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
+import GameSettings from "../gameSettings"
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +9,43 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let availableMoves = [];
+
+
+        //Forward Right Diagonal
+        let row = location.row + 1;
+        let col = location.col + 1;
+        for (let i = row, j = col; i <= 7, j <= 7; i++, j++) {
+            availableMoves.push(Square.at(i, j));
+        }
+
+
+        //Forward Left Diagonal
+        row = location.row + 1;
+        col = location.col - 1;
+        for (let i = row, j = col; i <= 7, j >= 0; i++, j--) {
+            availableMoves.push(Square.at(i, j));
+        }
+
+
+        //Backward Right Diagonal
+        row = location.row - 1;
+        col = location.col + 1
+        for (let i = row, j = col; i >= 0; i--, j++) {
+            availableMoves.push(Square.at(i, j));
+        }
+
+        //Backward Left Diagonal
+        row = location.row - 1;
+        col = location.col - 1
+        for (let i = row, j = col; i>0, j>0; i--, j--) {
+            availableMoves.push(Square.at(i, j));
+        }
+
+
+        console.log(availableMoves)
+
+        return availableMoves;
     }
 }
