2c9cc6edfcb9cff312a68ca5ae7ed21e6334a44d
preek0719 <preetha.krishnan@softwire.com>
2025-05-01 10:58:24 +0100

    added 1 test case and method for queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,41 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
+
 
 export default class Queen extends Piece {
     constructor(player) {
         super(player);
-    }
+    }   
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let queenAvailableMoves = [];
+
+        for (let i = location.row+1, j = location.col+1; i <= 7 && j <= 7; i++, j++) {
+            queenAvailableMoves.push(Square.at(i, location.col));
+            queenAvailableMoves.push(Square.at(location.row, j));
+            queenAvailableMoves.push(Square.at(i,j))
+        }
+        for (let i = location.row-1, j = location.col-1; i >= 0 && j >= 0; i--, j--) {
+          
+            queenAvailableMoves.push(Square.at(i, location.col));
+            queenAvailableMoves.push(Square.at(location.row, j));
+            queenAvailableMoves.push(Square.at(i,j))
+        }
+       
+        for (let i = location.row +1, j = location.col -1; i <= 7 && j >= 0; i++, j--) { 
+            
+            queenAvailableMoves.push(Square.at(i, j));
+        }
+       
+        for (let i = location.row - 1, j = location.col + 1; i >=0 && j<=7; i--, j++) { 
+            
+            queenAvailableMoves.push(Square.at(i, j));
+        }
+     
+        return queenAvailableMoves;
+        
     }
 }
+
